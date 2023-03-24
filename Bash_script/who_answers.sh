a1db1b7875476d16d41cc0933cdc8a8d14b1e483
Gribin Vadim <124711550+GribinVadim@users.noreply.github.com>
2023-03-24 10:18:44 +0300

    Update who_answers.sh

--- Bash_script/who_answers.sh
+++ Bash_script/who_answers.sh
@@ -2,11 +2,11 @@
 Day=$(Env LC_TIME=ru_RU.UTF-8 Date "+%A")
 date "+%d.%m.%Y  $Day"
 if [ -e students.txt ]; then
-	rm students.txt
+        rm students.txt
 else
-	echo "Списка студентов нет"
+        echo "Списка студентов нет"
 fi
 for students in Вадим Андрей Света Лера; do
-	echo "$students" >>students.txt
+        echo "$students" >>students.txt
 done
-echo "Преподаватель: Пётр" && echo "Список студентов:" && sort -d students.txt && echo "К доске идет:" && cat students.txt | shuf -n1
+echo "Преподаватель: Пётр" && echo "Список студентов:" && sort -d students.txt && echo "К доске идет:" && < students.txt shuf -n1
